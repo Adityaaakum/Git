7a8f134a0193900e9d2b1ed648311a45eb4eb683
Aditya <akaditya041@gmail.com>
2021-02-13 14:46:39 +0530

    private 1

---
+++ src/codeSimple/Aee.java
@@ -0,0 +1,42 @@
+package codeSimple;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Aee {
+	public static void main(String[] args) {
+		
+		
+		
+		Integer[] a1 = {1,2,3,2,1};
+		Integer[] a2 = {1,2,3,6,7};
+		
+		System.out.println(result(a1, a2));
+		
+}
+public static boolean result(Integer[]ar,Integer[]ar2)
+{
+	Set<Integer> li= new HashSet<Integer>(Arrays.asList(ar));
+	Set<Integer> li2= new HashSet<Integer>(Arrays.asList(ar2));
+	
+	if(!(li.size()==li2.size()))
+	{
+		return false;
+	}
+	
+	for(Object ob:li)
+	{
+		if(!(li2.contains(ob)))
+		{
+			return false;
+		}
+		
+	}
+	return true;
+	
+}
+
+
+
+}
